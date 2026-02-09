0618ed383cce46be565e9ac680aa8b5c9a747799
JackOfficial <musengimanajacques@gmail.com>
2026-02-09 13:43:52 +0200

    Blogging System

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\PartnerController;
 use App\Http\Controllers\OurServiceController;
+use App\Http\Controllers\SocialLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,6 +21,20 @@ Route::get('/services', [OurServiceController::class, 'services'])->name('servic
 Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
 Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
 
-Route::get('/dashboard', function () {
-    // ...
-})->middleware(['verified']);
+// Social login routes
+Route::get('/auth/redirect/{provider}', [SocialLoginController::class, 'redirect']);
+Route::get('/auth/callback/{provider}', [SocialLoginController::class, 'callback']);
+
+//Admin and super admin Routes
+Route::middleware(['auth', 'role:admin|super-admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', fn() => 'Admin Dashboard')->name('admin.dashboard');
+
+    Route::get('/', [AdminController::class, 'index'])->name('dashboard');
+
+    Route::resource('blogs', BlogController::class);
+
+    // Route::resource('users', UsersController::class);
+    Route::resource('partners', PartnerController::class);
+    Route::resource('organization', OrganizationController::class);
+
+});
