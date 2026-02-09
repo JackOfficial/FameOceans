13ab753a5c6b00234c5e94699cf54404d12e78fa
JackOfficial <musengimanajacques@gmail.com>
2026-02-09 20:30:44 +0200

    Blogging System

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,8 +1,7 @@
 <aside class="main-sidebar sidebar-dark-pink elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('admin.dashboard') }}" class="brand-link">
-        <img src="{{ asset('images/logo.png') }}" alt="FameOceans" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">FameOceans</span>
+        <img src="{{ asset('images/logo.png') }}" alt="FameOceans" class="brand-image elevation-3 w-25">
     </a>
 
     <!-- Sidebar -->
