08854c417a9411eb8177b46b8f5bfac1f58d9731
JackOfficial <musengimanajacques@gmail.com>
2026-02-09 18:16:06 +0200

    Blogging System

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Permissions
+        Permission::create(['name' => 'view posts']);
+        Permission::create(['name' => 'create posts']);
+        Permission::create(['name' => 'edit posts']);
+        Permission::create(['name' => 'delete posts']);
+
+        // Roles
+        $admin = Role::create(['name' => 'admin']);
+        $editor = Role::create(['name' => 'editor']);
+        $user = Role::create(['name' => 'user']);
+
+        // Assign permissions
+        $admin->givePermissionTo(Permission::all());
+        $editor->givePermissionTo(['view posts', 'create posts', 'edit posts']);
+        $user->givePermissionTo(['view posts']);
+    }
+}
