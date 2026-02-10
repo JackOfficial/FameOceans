a48d32b3f10ebaf3859bab903b7186eac01a2ede
JackOfficial <musengimanajacques@gmail.com>
2026-02-10 04:36:55 +0200

    Blogging System

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -91,7 +91,7 @@
     <div class="row w-100 justify-content-center">
         <div class="col-md-5 col-lg-4">
 
-            <img class="mb-0 d-block mx-auto"
+            <img class="my-2 d-block mx-auto"
                  style="width: 180px;"
                  src="{{ asset('images/FameOceans.png') }}"
                  alt="FameOceans Logo">
