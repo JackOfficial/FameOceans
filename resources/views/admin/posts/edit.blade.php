7730f5d849659687de31ae496a83102932a9e361
JackOfficial <musengimanajacques@gmail.com>
2026-02-10 06:47:04 +0200

    Blogging System

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -15,4 +15,33 @@
         </div>
     </div>
 </section>
+
+<script src="https://cdn.tiny.cloud/1/akrlpx2p28jit8egdeyeyu4q0osghccndfvgksphc8hfni44/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+
+<script>
+document.addEventListener('alpine:init', () => {
+    Alpine.data('tinymceEditor', () => ({
+        init(el, value = '') {
+            tinymce.init({
+                target: el,
+                height: 400,
+                plugins: 'lists link image table code wordcount',
+                toolbar: 'undo redo | styles | blocks | bold italic underline | alignleft aligncenter alignright alignjustify | indent outdent | bullist numlist | link image table | code',
+                setup(editor) {
+                    // Set initial value
+                    editor.on('init', function() {
+                        editor.setContent(value);
+                    });
+                    // Update Alpine when content changes
+                    editor.on('Change KeyUp', function() {
+                        el.value = editor.getContent();
+                        el.dispatchEvent(new Event('input')); // Livewire reacts
+                    });
+                }
+            });
+        }
+    }));
+});
+</script>
+
 @endsection
