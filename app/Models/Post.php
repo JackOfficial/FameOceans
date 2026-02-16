5c3e3f1f4853dd9bc7c98a2c9295f5a9b7746547
JackOfficial <musengimanajacques@gmail.com>
2026-02-16 04:14:08 +0200

    Blogging System

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -48,7 +48,7 @@ class Post extends Model
 
     public function tags(): BelongsToMany
     {
-        return $this->belongsToMany(Tag::class)->withTimestamps();
+        return $this->belongsToMany(Tag::class, 'post_tags')->withTimestamps();
     }
 
     public function comments(): HasMany
