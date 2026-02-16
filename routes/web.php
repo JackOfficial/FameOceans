d843aeeaa2a26765acd396d8840cfb5a6a05ed48
JackOfficial <musengimanajacques@gmail.com>
2026-02-16 02:22:20 +0200

    Blogging System

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\OurServiceController;
 use Illuminate\Support\Facades\Route;
 
 //Guest user routes
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);
 
 //Authenticated user routes
 Route::middleware(['auth', 'verified', 'role:user'])->group(function () { 
