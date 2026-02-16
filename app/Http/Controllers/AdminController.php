ebe8c4bdfdf7187214690b4b7cf22ef8a752d9a1
JackOfficial <musengimanajacques@gmail.com>
2026-02-16 06:05:01 +0200

    Blogging System

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\BlogCategory;
+use App\Models\Post;
 use Faker\Provider\Company;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
    public function index(){
-    $posts = BlogCategory::count();
+    $posts = Post::count();
     return view('admin.index', Compact('posts'));
    }
 }
